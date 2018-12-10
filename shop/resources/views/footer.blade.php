e13f3577a110bd50dc86d207bca8e46cb77234e9
admin <lydt42@wru.vn>
2018-12-11 00:06:11 +0700

    first commit

---
+++ shop/resources/views/footer.blade.php
@@ -0,0 +1,97 @@
+<!-- tin mới -->
+
+	<div class="tin-moi">
+		<div class="container">
+			<div class="block_content ">
+				<div class="row">
+					<div class="col-md-4">
+						<h6>ĐĂNG KÝ ĐỂ NHẬN TIN MỚI</h6>
+					</div>
+					<div class="col-md-8">
+						<div class="input-group">
+							<input type="email" value="" placeholder="Nhập Email của bạn ..." name="EMAIL" id="mail" class="newsletter-input form-control input-lg" aria-label="Nhập Email của bạn ...">
+							<span class="input-group-btn">
+								<button id="subscribe" class="btn btn-newsletter" type="submit">
+									<span>Theo dõi</span>
+								</button>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- tin mới -->
+
+
+	<div class="container">
+		<div id="footer" class="color-div ">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 col-sm-12 col-xs-12 ">
+						<div class="widget">
+							<img class="text-center" src="source/assets/dest/images/shop_images_footer.png" alt="">
+							<p>Nhà hàng Leo Food thiết kế theo phong cách vừa truyền thống vừa mang nét hiện đại. Kiến trúc sang trọng kết hợp với khung cảnh thiên nhiên tạo cảm giác đặc biệt hơn cho du khách khi thưởng thức ẩm thực tại đây.</p>
+						</div>
+					</div>
+
+					<div class="col-md-3 col-sm-4 col-xs-12 ">
+						<div class="widget">
+							<h4 class="widget-title">Liên hệ</h4>
+							<div>
+								<ul>
+									<li><i class="fas fa-phone"></i><a href="">Di động : 190000000</a></li>
+									<li><a href="">Email : lydt42@wru.vn</a></li>
+									<li><p> 175 Tây Sơn, Đống Đa, Hà Nội</p></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4 col-xs-12 ">
+						<div class="col-sm-10">
+							<div class="widget">
+								<h4 class="widget-title">Thông tin</h4>
+								<div>
+									<div class="contact-info">
+										<ul>
+											<li>
+												<a href="">Hướng dẫn mua hàng</a>
+											</li>
+											<li>
+												<a href="">Chính sách vận chuyển</a>
+											</li>
+											<li>
+												<a href="">Điều khoản dịch vụ</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4 col-xs-12 ">
+						<div class="widget">
+							<h4 class="widget-title">Leo Food</h4>
+								<!-- <form action="#" method="post">
+									<input type="email" name="your_email">
+									<button class="pull-right" type="submit">Subscribe <i class="fa fa-chevron-right"></i></button>
+								</form> -->
+							</div>
+						</div>
+					</div> <!-- .row -->
+				</div> <!-- .container -->
+			</div>
+		</div> <!-- #footer -->
+		<div class="container">
+			<div class="copyright">
+				<p class="pull-left">Privacy policy. (&copy;) 2018</p>
+				<p class="pull-right pay-options">
+					<a href="#"><img src="source/assets/dest/images/pay/master.jpg" alt="" /></a>
+					<a href="#"><img src="source/assets/dest/images/pay/pay.jpg" alt="" /></a>
+					<a href="#"><img src="source/assets/dest/images/pay/visa.jpg" alt="" /></a>
+					<a href="#"><img src="source/assets/dest/images/pay/paypal.jpg" alt="" /></a>
+				</p>
+				<div class="clearfix"></div>
+			</div> <!-- .container -->
+		</div> <!-- .copyright -->
---
+++ shop/resources/views/page/tintuc.blade.php
@@ -0,0 +1,79 @@
+@extends('master')
+@section('content')
+<style type="text/css" media="screen">
+.contentcat {
+    width: 100%;
+    margin: 50px auto 0;
+	
+</style>
+<div class="container">
+	<div class="">
+		<div class="row contentcat">
+			<div class="col-md-3">
+				<a href=""><img width="310" height="165" src="http://spaghettibox.com/wp-content/uploads/2018/12/huong-dan-dat-ban-to-chuc-sinh-nhat-tai-sbox-310x165.jpg" class="attachment-tie-medium size-tie-medium wp-post-image tie-appear" alt="hướng dẫn đặt bàn tổ chức sinh nhật tại S'Box"></a>
+			</div>
+			<div class="col-md-9">
+				<a href="http://spaghettibox.com/huong-dan-dat-ban-chuc-sinh-nhat-tai-sbox.html"><h2>Hướng dẫn đặt bàn tổ chức sinh nhật tại S’Box</h2></a>
+				<p class="post-meta" style="margin: 10px 0;">
+				<span class="tie-date"><i class="fa fa-clock-o"></i>5 ngày ago</span>
+				<span class="post-cats"><i class="fa fa-folder"></i><a href="{{route('tintuc')}}" rel="category tag">Tin tức</a></span>
+				</p>
+				<p style="font-family: arial;color: black;">Sinh nhật luôn là một sự kiện đặc biệt đối với mỗi người ở mọi độ tuổi. Vì vậy,&nbsp;ai cũng muốn tổ chức sinh nhật theo cách thật đặc biệt, mỗi năm mỗi khác.</p>
+			</div>
+		</div>
+		<div class="row contentcat">
+			<div class="col-md-3">
+				<a href=""><img width="310" height="165" src="http://spaghettibox.com/wp-content/uploads/2018/11/loi-ich-suc-khoe-cua-uc-ga-310x165.jpg" class="attachment-tie-medium size-tie-medium wp-post-image tie-appear" alt="Lợi ích sức khỏe của thịt ức gà"></a>
+			</div>
+			<div class="col-md-9">
+				<a href="http://spaghettibox.com/huong-dan-dat-ban-chuc-sinh-nhat-tai-sbox.html"><h2>Hướng dẫn đặt bàn tổ chức sinh nhật tại S’Box</h2></a>
+				<p class="post-meta" style="margin: 10px 0;">
+				<span class="tie-date"><i class="fa fa-clock-o"></i>5 ngày ago</span>
+				<span class="post-cats"><i class="fa fa-folder"></i><a href="{{route('tintuc')}}" rel="category tag">Tin tức</a></span>
+				</p>
+				<p style="font-family: arial;color: black;">Sinh nhật luôn là một sự kiện đặc biệt đối với mỗi người ở mọi độ tuổi. Vì vậy,&nbsp;ai cũng muốn tổ chức sinh nhật theo cách thật đặc biệt, mỗi năm mỗi khác.</p>
+			</div>
+		</div>
+		<div class="row contentcat">
+			<div class="col-md-3">
+				<a href=""><img width="310" height="165" src="http://spaghettibox.com/wp-content/uploads/2018/12/huong-dan-dat-ban-to-chuc-sinh-nhat-tai-sbox-310x165.jpg" class="attachment-tie-medium size-tie-medium wp-post-image tie-appear" alt="hướng dẫn đặt bàn tổ chức sinh nhật tại S'Box"></a>
+			</div>
+			<div class="col-md-9">
+				<a href="http://spaghettibox.com/huong-dan-dat-ban-chuc-sinh-nhat-tai-sbox.html"><h2>Hướng dẫn đặt bàn tổ chức sinh nhật tại S’Box</h2></a>
+				<p class="post-meta" style="margin: 10px 0;">
+				<span class="tie-date"><i class="fa fa-clock-o"></i>5 ngày ago</span>
+				<span class="post-cats"><i class="fa fa-folder"></i><a href="{{route('tintuc')}}" rel="category tag">Tin tức</a></span>
+				</p>
+				<p style="font-family: arial;color: black;">Sinh nhật luôn là một sự kiện đặc biệt đối với mỗi người ở mọi độ tuổi. Vì vậy,&nbsp;ai cũng muốn tổ chức sinh nhật theo cách thật đặc biệt, mỗi năm mỗi khác.</p>
+			</div>
+		</div>
+		<div class="row contentcat">
+			<div class="col-md-3">
+				<a href=""><img width="310" height="165" src="http://spaghettibox.com/wp-content/uploads/2018/12/huong-dan-dat-ban-to-chuc-sinh-nhat-tai-sbox-310x165.jpg" class="attachment-tie-medium size-tie-medium wp-post-image tie-appear" alt="hướng dẫn đặt bàn tổ chức sinh nhật tại S'Box"></a>
+			</div>
+			<div class="col-md-9">
+				<a href="http://spaghettibox.com/huong-dan-dat-ban-chuc-sinh-nhat-tai-sbox.html"><h2>Hướng dẫn đặt bàn tổ chức sinh nhật tại S’Box</h2></a>
+				<p class="post-meta" style="margin: 10px 0;">
+				<span class="tie-date"><i class="fa fa-clock-o"></i>5 ngày ago</span>
+				<span class="post-cats"><i class="fa fa-folder"></i><a href="{{route('tintuc')}}" rel="category tag">Tin tức</a></span>
+				</p>
+				<p style="font-family: arial;color: black;">Sinh nhật luôn là một sự kiện đặc biệt đối với mỗi người ở mọi độ tuổi. Vì vậy,&nbsp;ai cũng muốn tổ chức sinh nhật theo cách thật đặc biệt, mỗi năm mỗi khác.</p>
+			</div>
+		</div>
+		<div class="row contentcat">
+			<div class="col-md-3">
+				<a href=""><img width="310" height="165" src="http://spaghettibox.com/wp-content/uploads/2018/12/huong-dan-dat-ban-to-chuc-sinh-nhat-tai-sbox-310x165.jpg" class="attachment-tie-medium size-tie-medium wp-post-image tie-appear" alt="hướng dẫn đặt bàn tổ chức sinh nhật tại S'Box"></a>
+			</div>
+			<div class="col-md-9">
+				<a href="http://spaghettibox.com/huong-dan-dat-ban-chuc-sinh-nhat-tai-sbox.html"><h2>Hướng dẫn đặt bàn tổ chức sinh nhật tại S’Box</h2></a>
+				<p class="post-meta" style="margin: 10px 0;">
+				<span class="tie-date"><i class="fa fa-clock-o"></i>5 ngày ago</span>
+				<span class="post-cats"><i class="fa fa-folder"></i><a href="{{route('tintuc')}}" rel="category tag">Tin tức</a></span>
+				</p>
+				<p style="font-family: arial;color: black;">Sinh nhật luôn là một sự kiện đặc biệt đối với mỗi người ở mọi độ tuổi. Vì vậy,&nbsp;ai cũng muốn tổ chức sinh nhật theo cách thật đặc biệt, mỗi năm mỗi khác.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+@endsection
